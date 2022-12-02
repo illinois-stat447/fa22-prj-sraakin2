3332a31d9387c89e0d622f659d68fdab4ce26987
Shadman-Raakin <shadmanr678@gmail.com>
2022-12-01 19:51:58 -0600

    cancel

--- Covid-tracker/app.R
+++ Covid-tracker/app.R
@@ -1,99 +1,83 @@
 
 
-#library(shiny)
+library(shiny)
 
-# Define UI for application that draws a histogram
-#ui <- navbarPage(
- # theme = bs_theme(bootswatch = "darkly"),
-  #title = "Page",
-  #tabPanel("App", 
-#            titlePanel("2020-2022 UIUC Covid Tracker"),
-#            sidebarLayout(
-#              # sidebarPanel(
-#                selectInput(inputId = "groups", label = "Group", Groups), 
-#                checkboxInput(inputId = "positivity", label = "Positivity Rate"),
-#                dateRangeInput(inputId = "date", label = "Date Range",
-#                               start  = "2020-08-17",
-#                               end    = "2021-11-27",
-#                               min    = "2020-08-17",
-#                               max    = "2021-11-27",
-#                               format = "mm/dd/yy",
-#                               separator = " - "),
-#              ),
-#              
-#              mainPanel(plotOutput("selectPlot"), plotOutput("sliderPlot")))))
-# 
-# server <- function(input, output) {
-#   
-#   daterange <- reactive({
-#     Updated_Covid_Dataset %>% filter(Updated_Covid_Dataset$Dates >= input$date[1] 
-#                                   & Updated_Covid_Dataset$Dates <= input$date[2])
-#   })
-#   
-#   tablereact <- reactive({ 
-#     if (input$groups == 'Undergraduates') { 
-#       uiuc_covid_updated %>% select(Dates, undergradCases, undergradTests, UG_Positivity_rate)
-#     } else if (input$groups == 'Graduates') { 
-#       uiuc_covid_updated %>% select(Dates, gradCases, gradTests, Grad_Positivity_rate)
-#     } else if (input$groups == 'Faculty/Staff') { 
-#       uiuc_covid_updated %>% select(Dates, facStaffCases, facStaffTests, Faculty_Positivity_rate)
-#     } 
-#   })
-#   
-#   
-#   output$table = renderDataTable(tablereact())
-#   
-#   output$selectPlot <- renderPlot({
-#     
-#     if (input$groups == 'Undergraduates') { 
-#       cases_plot = ggplot(data = daterange(), aes(x = Dates, y = undergradCases)) + 
-#         geom_col(fill = 'blue', alpha = 0.6) + 
-#         theme_minimal(base_size = 14) +
-#         xlab("Date") + ylab("Daily Cases") + scale_x_date(date_labels = "%Y-%m-%d")
-#       cases_plot
-#       
-#     } else if (input$groups == 'Graduates') { 
-#       cases_plot = ggplot(data = daterange(), aes(x = Dates, y = gradCases)) + 
-#         geom_col(fill = 'dark red', alpha = 0.6) + 
-#         theme_minimal(base_size = 14) +
-#         xlab("Date") + ylab("Daily Cases") + scale_x_date(date_labels = "%Y-%m-%d")
-#       cases_plot
-#       
-#     } else if (input$groups == 'Faculty/Staff') { 
-#       cases_plot = ggplot(data = daterange(), aes(x = Dates, y = facStaffCases)) + 
-#         geom_col(fill = 'dark green', alpha = 0.6) + 
-#         theme_minimal(base_size = 14) +
-#         xlab("Date") + ylab("Daily Cases") + scale_x_date(date_labels = "%Y-%m-%d")
-#       cases_plot
-#     }
-#     
-#   })
-#   
-#   
-#   checkreactive <- reactive({ 
-#     if (input$positivity) { 
-#       if (input$groups == 'Undergraduates') { 
-#         pr_plot <- ggplot(data = daterange(), aes(x = Dates, y = UG_Positivity_rate)) + 
-#           geom_line() + xlab("Date") + ylab("Positivity Rate (%)")
-#         pr_plot
-#       } else if (input$groups == 'Graduates') { 
-#         pr_plot <- ggplot(data = daterange(), aes(x = Dates, y = Grad_Positivity_rate)) + 
-#           geom_line() + xlab("Date") + ylab("Positivity Rate (%)")
-#         pr_plot
-#       } else if (input$groups == 'Faculty/Staff') { 
-#         pr_plot <- ggplot(data = daterange(), aes(x = Dates, y = Faculty_Positivity_rate)) + 
-#           geom_line() + xlab("Date") + ylab("Positivity Rate (%)") 
-#         pr_plot
-#       }
-#     }
-#     
-#   })
-#   
-#   output$sliderPlot <- renderPlot({ 
-#     checkreactive()
-#   })
-#   
-# }
-# 
-# # Run the application 
-# shinyApp(ui = ui, server = server)
+ui <- navbarPage(
+  theme = bs_theme(bootswatch = "darkly"),
+  title = "Main Page",
+  tabPanel("Main App",
+            titlePanel("2020-2022 UIUC Covid Tracker"),
+            sidebarLayout(
+              sidebarPanel(
+                selectInput(inputId = "groups", label = "Group", Groups),
+                checkboxInput(inputId = "positivity", label = "Positivity Rate"),
+                dateRangeInput(inputId = "time", label = "Date Range",
+                               start  = "2020-08-17",
+                               end    = "2022-10-13",
+                               min    = "2020-08-17",
+                               max    = "2022-10-13",
+                               format = "mm/dd/yy",
+                               separator = " - "),
+              ),
+
+              mainPanel(plotOutput("selectPlot"), plotOutput("sliderPlot"))))
+)
+
+ server <- function(input, output) {
+   daterange <- reactive({
+     Updated_Covid_Dataset %>% filter(Updated_Covid_Dataset$time >= input$time[1] 
+                                   & Updated_Covid_Dataset$time <= input$time[2])
+   })
+   
+   output$selectPlot <- renderPlot({
+     
+     if (input$groups == 'Undergraduates') { 
+       cases_plot = ggplot(data = daterange(), aes(x = time, y = undergradCases)) + 
+         geom_col(fill = 'blue', alpha = 0.6) + 
+         theme_minimal(base_size = 14) +
+         xlab("Date") + ylab("Daily Cases") + scale_x_date(date_labels = "%Y-%m-%d")
+       cases_plot
+       
+     } else if (input$groups == 'Graduates') { 
+       cases_plot = ggplot(data = daterange(), aes(x = time, y = gradCases)) + 
+         geom_col(fill = 'dark red', alpha = 0.6) + 
+         theme_minimal(base_size = 14) +
+         xlab("Date") + ylab("Daily Cases") + scale_x_date(date_labels = "%Y-%m-%d")
+       cases_plot
+       
+     } else if (input$groups == 'Faculty/Staff') { 
+       cases_plot = ggplot(data = daterange(), aes(x = time, y = facStaffCases)) + 
+         geom_col(fill = 'dark green', alpha = 0.6) + 
+         theme_minimal(base_size = 14) +
+         xlab("Date") + ylab("Daily Cases") + scale_x_date(date_labels = "%Y-%m-%d")
+       cases_plot
+     }
+     
+   })
+   
+   
+   checkreactive <- reactive({ 
+     if (input$positivity) { 
+       if (input$groups == 'Undergraduates') { 
+         PR_plot <- ggplot(data = daterange(), aes(x = time, y = UG_Positivity_Rate)) + 
+           geom_line() + xlab("Date") + ylab("Positivity Rate (%)")
+         PR_plot
+       } else if (input$groups == 'Graduates') { 
+         PR_plot <- ggplot(data = daterange(), aes(x = time, y = Grad_Positivity_Rate)) + 
+           geom_line() + xlab("Date") + ylab("Positivity Rate (%)")
+         PR_plot
+       } else if (input$groups == 'Faculty/Staff') { 
+         PR_plot <- ggplot(data = daterange(), aes(x = time, y = Faculty_Positivity_Rate)) + 
+           geom_line() + xlab("Date") + ylab("Positivity Rate (%)") 
+         PR_plot
+       }
+     }
+     
+   })
+   
+   output$sliderPlot <- renderPlot({ 
+     checkreactive()
+   })
+   
+ }
+ shinyApp(ui = ui, server = server)
